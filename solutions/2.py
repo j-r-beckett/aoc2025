e1224d193a0d9985d8de71623715758b343074e5
Jimmy Beckett <jimmybeckett1+github@gmail.com>
2025-12-01 22:04:29 -0800

    Rename

--- solutions/2.py
+++ solutions/2.py
@@ -5,7 +5,7 @@ def parse(raw):
     return [tuple(int(x) for x in r.split("-")) for r in raw.split(",")]
 
 
-def count_invalid(ranges, is_invalid):
+def sum_invalid(ranges, is_invalid):
     return sum(
         n for start, end in ranges for n in range(start, end + 1) if is_invalid(n)
     )
@@ -15,14 +15,14 @@ def part1(ranges):
     def is_invalid(id):
         return bool(re.search(r"^(.+)\1$", str(id)))
 
-    return count_invalid(ranges, is_invalid)
+    return sum_invalid(ranges, is_invalid)
 
 
 def part2(ranges):
     def is_invalid(id):
         return bool(re.search(r"^(.+)\1+$", str(id)))
 
-    return count_invalid(ranges, is_invalid)
+    return sum_invalid(ranges, is_invalid)
 
 
 if __name__ == "__main__":
