ceb4d3b4971158ac43f97e570d5adf20d69c9006
Jimmy Beckett <jimmybeckett1+github@gmail.com>
2025-12-06 22:45:04 -0800

    Day 7

---
+++ solutions/7.py
@@ -0,0 +1,54 @@
+def parse(raw):
+    return [list(line.strip()) for line in raw.split("\n")]
+
+
+def part1(diagram):
+    for c in range(len(diagram[0])):
+        if diagram[0][c] == "S":
+            diagram[0][c] = "|"
+    splits = 0
+    for row in range(len(diagram) - 1):
+        for col in range(len(diagram[0])):
+            if diagram[row][col] == "|":
+                if diagram[row + 1][col] == "^":
+                    splits += 1
+                    if col > 0 and diagram[row + 1][col - 1] != "^":
+                        diagram[row + 1][col - 1] = "|"
+                    if col < len(diagram[0]) - 1 and diagram[row + 1][col + 1] != "^":
+                        diagram[row + 1][col + 1] = "|"
+                else:
+                    diagram[row + 1][col] = "|"
+    return splits
+
+
+def part2(diagram):
+    cache = {}
+
+    def timelines(row, col):
+        if row == len(diagram) - 1:
+            return 1
+        if (row, col) not in cache:
+            if diagram[row + 1][col] == "^":
+                result = 0
+                if col > 0 and diagram[row + 1][col - 1] != "^":
+                    result += timelines(row + 1, col - 1)
+                if col < len(diagram[0]) - 1 and diagram[row + 1][col + 1] != "^":
+                    result += timelines(row + 1, col + 1)
+            else:
+                result = timelines(row + 1, col)
+            cache[(row, col)] = result
+        return cache[(row, col)]
+
+    return timelines(0, diagram[0].index("S"))
+
+
+if __name__ == "__main__":
+    day = 7
+    with open(f"inputs/{day}.example.txt") as f:
+        raw = f.read().strip()
+        print(f"Example part 1: {part1(parse(raw))}")
+        print(f"Example part 2: {part2(parse(raw))}")
+    with open(f"inputs/{day}.txt") as f:
+        raw = f.read().strip()
+        print(f"Part 1: {part1(parse(raw))}")
+        print(f"Part 2: {part2(parse(raw))}")
